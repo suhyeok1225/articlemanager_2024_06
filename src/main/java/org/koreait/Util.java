6688c871b21f3a28ccdf65db64407a19e0389512
suhyeok1225 <chaesh1225@gmail.com>
2024-07-02 13:42:59 +0900

    날짜 기능 추가

---
+++ src/main/java/org/koreait/Util.java
@@ -0,0 +1,14 @@
+package org.koreait;
+
+import java.time.DateTimeException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Util {
+    public static String getNow(){
+        LocalDateTime now = LocalDateTime.now();
+        String formateNow = now.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        return formateNow;
+    }
+
+}
