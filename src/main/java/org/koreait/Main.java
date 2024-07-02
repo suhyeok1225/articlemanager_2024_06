325021df0ff0c7a976edd6c684726f61b42f4d19
suhyeok1225 <chaesh1225@gmail.com>
2024-07-02 15:19:16 +0900

    종복제거

--- src/main/java/org/koreait/Main.java
+++ src/main/java/org/koreait/Main.java
@@ -6,11 +6,12 @@ import java.util.Scanner;
 
 public class Main {
     public static List<Article> articles = new ArrayList<>();
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("==프로그램 시작==");
         makeTestData();
-        int lastArticleId = 0;
+        int lastArticleId = 3;
 
         while (true) {
             System.out.print("명령어) ");
@@ -42,26 +43,22 @@ public class Main {
                 if (articles.size() == 0) {
                     System.out.println("아무것도 없어");
                 } else {
-                    System.out.println("  번호   /    날짜   /   제목   /   내용   ");
+                    System.out.println("  번호   /       날짜     /   제목    /   내용   ");
                     for (int i = articles.size() - 1; i >= 0; i--) {
                         Article article = articles.get(i);
                         if (Util.getNow().split(" ")[0].equals(article.getRegDate().split(" ")[0])) {
-                            System.out.printf("  %d   /   %s      /   %s   /   %s  \n", article.getId(), article.getRegDate().split(" ")[1], article.getTitle(), article.getBody());
+                            System.out.printf("   %d     /   %s   /   %s   /   %s  \n", article.getId(), article.getRegDate().split(" ")[1], article.getTitle(), article.getBody());
                         } else {
-                            System.out.printf("  %d   /   %s      /   %s   /   %s  \n", article.getId(), article.getRegDate().split(" ")[0], article.getTitle(), article.getBody());
+                            System.out.printf("   %d     /   %s   /   %s   /   %s  \n", article.getId(), article.getRegDate().split(" ")[0], article.getTitle(), article.getBody());
                         }
                     }
                 }
+
             } else if (cmd.startsWith("article detail")) {
                 System.out.println("==게시글 상세보기==");
                 int id = Integer.parseInt(cmd.split(" ")[2]);
-                Article foundArticle = null;
-                for (Article article : articles) {
-                    if (article.getId() == id) {
-                        foundArticle = article;
-                        break;
-                    }
-                }
+                Article foundArticle = getArticleById(id);
+
                 if (foundArticle == null) {
                     System.out.println("해당 게시글은 없습니다");
                     continue;
@@ -76,16 +73,9 @@ public class Main {
 
                 int id = Integer.parseInt(cmd.split(" ")[2]);
 
-                Article foundArticle = null;
-
-                for (Article article : articles) {
-                    if (article.getId() == id) {
-                        foundArticle = article;
-                        break;
-                    }
-                }
+                Article foundArticle = getArticleById(id);
                 if (foundArticle == null) {
-                    System.out.println("해당 게시글은 없습니다");
+                    System.out.printf("해당 %d 번 게시글은 없습니다\n", id);
                     continue;
                 }
                 articles.remove(foundArticle);
@@ -94,14 +84,8 @@ public class Main {
                 System.out.println("==게시글 수정==");
                 int id = Integer.parseInt(cmd.split(" ")[2]);
 
-                Article foundArticle = null;
+                Article foundArticle = getArticleById(id);
 
-                for (Article article : articles) {
-                    if (article.getId() == id) {
-                        foundArticle = article;
-                        break;
-                    }
-                }
                 if (foundArticle == null) {
                     System.out.println("해당 게시글은 없습니다");
                     continue;
@@ -128,11 +112,20 @@ public class Main {
 
     }
 
+    private static Article getArticleById(int id) {
+        for (Article article : articles) {
+            if (article.getId() == id) {
+                return article;
+            }
+        }
+        return null;
+    }
+
     private static void makeTestData() {
         System.out.println("테스트 데이터 생성");
-        articles.add(new Article(1, "2024-11-11","2024-11-12","제목1","내용1"));
-        articles.add(new Article(2, "2024-12-11","2024-12-12","제목2","내용2"));
-        articles.add(new Article(3, "2024-10-11","2024-10-12","제목3","내용3"));
+        articles.add(new Article(1, "2024-11-11", "2024-11-12", "제목1", "내용1"));
+        articles.add(new Article(2, "2024-12-11", "2024-12-12", "제목2", "내용2"));
+        articles.add(new Article(3, "2024-10-11", "2024-10-12", "제목3", "내용3"));
     }
 
 }
