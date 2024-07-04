f4b656fc3a37de535d9a57128a3001249de79889
suhyeok1225 <chaesh1225@gmail.com>
2024-07-04 11:42:13 +0900

    게시글 작성시 작성자에 대한 정보가 같이 남도록

--- src/main/java/org/koreait/dto/Article.java
+++ src/main/java/org/koreait/dto/Article.java
@@ -4,13 +4,22 @@ public class Article extends Dto {
     private String updateDate;
     private String title;
     private String body;
-
-    public Article(int id, String regDate, String updateDate, String title, String body) {
+    private int memberId;
+    public Article(int id, String regDate, String updateDate, int memberId, String title, String body) {
         this.id = id;
         this.regDate = regDate;
         this.updateDate = updateDate;
+        this.memberId = memberId;
         this.title = title;
         this.body = body;
+    }
+
+    public int getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(int memberId) {
+        this.memberId = memberId;
     }
 
     public String getRegDate() {
